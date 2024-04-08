1b8de46f9de99e445ab2382a172d955535cfba2b
alexba2004 <j.alejandro.briones.a22@gmail.com>
2024-04-08 02:44:39 -0600

    add: groceries api rest

---
+++ src/dao/products.dao.js
@@ -0,0 +1,40 @@
+import Product from "../models/product.model.js";
+const productDao = {};
+productDao.getAll = async (req, res) => {
+  const products = await Product.find();
+  return products;
+};
+
+productDao.getOne = async (barcode) => {
+  const product = await Product.findOne({ barcode: barcode });
+  return product;
+};
+
+productDao.insertOne = async (product) => {
+  const productSaved = new Product(product);
+  await productSaved.save();
+  return true;
+};
+
+productDao.updateOne = async (barcode, product) => {
+  const updateOne = await Product.findOneAndUpdate(
+    { barcode: barcode },
+    product
+  );
+  if (updateOne != null) {
+    return updateOne;
+  } else {
+    return false;
+  }
+};
+
+productDao.deleteOne = async (barcode) => {
+  const deleteOne = await Product.findOneAndDelete({ barcode: barcode });
+  if (deleteOne != null) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default productDao;
---
+++ src/routes/product.routes.js
@@ -0,0 +1,11 @@
+import Router from "express";
+import productController from "../controllers/product.controller.js";
+const router = Router();
+
+router.get("/", productController.getAll);
+router.get("/:barcode", productController.getOne);
+router.post("/", productController.insertOne);
+router.put("/:barcode", productController.updateOne);
+router.delete("/delete/:barcode", productController.deleteOne);
+
+export default router;
